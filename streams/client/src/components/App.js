29821a2e413914455307f8d5b08b5c6bcbab382e
EANimesha <nimeshadilini999@gmail.com>
2020-03-07 00:11:25 +0530

    created a project like twitch.tv and added routes

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {BrowserRouter,Route} from 'react-router-dom';
+import StreamCreate from './Streams/StreamCreate'
+import StreamEdit from './Streams/StreamEdit'
+import StreamDelete from './Streams/StreamDelete'
+import StreamList from './Streams/StreamList'
+import StreamShow from './Streams/StreamShow'
+import Header from './header'
+
+const App=()=> {
+    return (
+        <div className="ui container">
+            
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/show" component={StreamShow}/>
+                    <Route path="/streams/create" component={StreamCreate}/>
+                    <Route path="/streams/edit" component={StreamEdit}/>
+                    <Route path="/streams/delete" component={StreamDelete}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
