6f64c80f2ee245c368a256f504eccc700de027ee
EANimesha <nimeshadilini999@gmail.com>
2020-03-09 22:54:00 +0530

    implemented all restful actions with react and redux

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,26 +1,27 @@
 import React from 'react'
-import {BrowserRouter,Route} from 'react-router-dom';
+import {Router,Route} from 'react-router-dom';
 import StreamCreate from './Streams/StreamCreate'
 import StreamEdit from './Streams/StreamEdit'
 import StreamDelete from './Streams/StreamDelete'
 import StreamList from './Streams/StreamList'
 import StreamShow from './Streams/StreamShow'
 import Header from './header'
+import history from '../history'
 
 const App=()=> {
     return (
         <div className="ui container">
             
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header/>
                     <Route path="/" exact component={StreamList}/>
                     <Route path="/streams/show" component={StreamShow}/>
                     <Route path="/streams/create" component={StreamCreate}/>
-                    <Route path="/streams/edit" component={StreamEdit}/>
+                    <Route path="/streams/edit/:id" component={StreamEdit}/>
                     <Route path="/streams/delete" component={StreamDelete}/>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     )
 }
