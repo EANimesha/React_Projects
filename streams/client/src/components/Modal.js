d97d4f38e1218f34215afe86d72c443405a77001
EANimesha <nimeshadilini999@gmail.com>
2020-04-11 20:09:17 +0530

    updated ui of modal and added hiding feature when clicked out of modal

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import history from '../history';
 
 const Modal=props=>{
     return ReactDom.createPortal(
-        <div className="ui dimmer modals visible active">
+        <div onClick={()=>history.push('/')} className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
-                This is a sample modal
+                <div className="header">Delete stream</div>
+                <div className="content">
+                    Are you sure you want to delete the stream
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
-        </div>,
+        </div>, 
         document.querySelector('#modal')
     )
 }
