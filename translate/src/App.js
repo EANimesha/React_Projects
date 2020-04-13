eba1754504c4d1247c8d18f0e33e17933ff86645
EANimesha <nimeshadilini999@gmail.com>
2020-04-13 23:51:43 +0530

    implemented select language feature

--- translate/src/App.js
+++ translate/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     app
-    </div>
-  );
+class App extends React.Component {
+  state={
+    language:'english'
+  }
+  onLanguageChange=(language)=>{
+    this.setState({language})
+  }
+  render(){
+    return (
+      <div>
+       <div>
+         Select A language:
+         <i className="flag us" onClick={()=>this.onLanguageChange('english')}></i>
+         <i className="flag nl" onClick={()=>this.onLanguageChange('dutch')}></i>
+       </div>
+       {this.state.language}
+      </div>
+    );
+  }
 }
 
 export default App;
